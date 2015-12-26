97ea57a6782828beb7836010f9470d5b0784c285
minglho <mingho@cal.berkeley.edu>
2015-12-25 22:40:27 -0800

    Added blurb about BMI and reformatted text.

--- ui.R
+++ ui.R
@@ -10,7 +10,7 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      selectInput("system", "Choose measurement system:",
+      selectInput("system", "Choose measurement system for your input:",
                   c("Imperial" = "Imperial",
                     "Metric" = "Metric")),
       uiOutput("getWt"),
@@ -21,9 +21,23 @@ shinyUI(fluidPage(
 
     # Show a plot of the generated distribution
     mainPanel(
-      h4('Your BMI is'),
+      tags$html(
+          tags$body(
+              p("Body mass index (BMI) is a measure of body fat based on height and weight that applies to adult men and women."),
+              p("Although BMI can be used for most adults, it does have some limits:")
+          ),
+          tags$li(
+              p("It may overestimate body fat in athletes and others who have a muscular build.")
+          ),
+          tags$li(
+              p("It may underestimate body fat in older persons and others who have lost muscle.")
+          )
+      ),
+      br(),
+      p(strong("Input your weight and height to the right to calculate your BMI and find out if you are overweight.")),
+      p(strong('Your BMI is')),
       verbatimTextOutput("BMI"),
-      h4('As a measure of body fat, your BMI indicates that you are'),
+      p(strong('As a measure of body fat, your BMI indicates that you are')),
       verbatimTextOutput("BMIcategory")
     )
   )
